e5dcd8ff5020d7fcdb655773c9301b1ddc642f1b
Serdar <serdarcholukov@yandex.com>
2021-08-25 20:12:28 +0500

    commit1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+        <h1>Commit 1</h1>
         <h1>Home</h1>
         home Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
         minus unde voluptas. Placeat, nulla nemo dolorum nisi sit vel nobis
